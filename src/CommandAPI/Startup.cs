1dc14894b9cb727db3cd782c2d1567f2871c62ab
yontenc <yontenchoden1991@gmail.com>
2021-04-09 11:54:46 +0600

    Migration Added and SQLRepo added

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -8,18 +8,27 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using CommandAPI.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace CommandAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration {get;}
+        public Startup(IConfiguration configuration)
+          {
+            Configuration = configuration;
+          }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             //Add service
+            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql
+                (Configuration.GetConnectionString("PostgreSqlConnection")));
             services.AddControllers();
-            services.AddScoped<ICommandAPIRepo,MockCommandAPIRepo>();
+            services.AddScoped<ICommandAPIRepo,SqlCommandAPIRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
